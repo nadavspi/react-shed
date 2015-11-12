8fe521fde89824cf52ac91309f73bc551a0487aa
Nadav Spiegelman <me@nadav.name>
2015-11-11 22:13:39 -0500

    Add range input for tempo control

--- src/Metronome/TempoControl.js
+++ src/Metronome/TempoControl.js
@@ -5,12 +5,24 @@ export const TempoControl = ({ handleChange, value }) => {
     <label>
       Tempo: {' '}
       <input
+	max="400"
+	min="40"
 	onChange={(e) => handleChange(e.target.value)}
+	step="10"
 	type="number"
 	value={value}
       />
       {' '}
       BPM
+
+      <input
+	max="400"
+	min="40"
+	onChange={(e) => handleChange(e.target.value)}
+	step="10"
+	type="range"
+	value={value}
+      />
     </label>
   );
 }
